0c450afa74bdc1e2d76ca33c926f1930d2525316
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-04-27 19:48:15 +0100

    added social links

---
+++ src/components/socials.js
@@ -0,0 +1,50 @@
+import {
+  GithubLogo,
+  LinkedinLogo,
+  TwitterLogo,
+  InstagramLogo,
+} from "@phosphor-icons/react";
+import { StyledSocials } from "../styles/socials.style";
+
+const Socials = () => {
+  return (
+    <StyledSocials>
+      <ul>
+        <li>
+          <a
+            href="https://github.com/Jessepelumi"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GithubLogo size={24} />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/jeolad/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <LinkedinLogo size={24} />
+          </a>
+        </li>
+        <li>
+          <a href="https://x.com/jeolad_?s=09" target="_blank" rel="noreferrer">
+            <TwitterLogo size={24} />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/jeolad_?igsh=YTh2OHM0ZmJlNXdx"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <InstagramLogo size={24} />
+          </a>
+        </li>
+      </ul>
+    </StyledSocials>
+  );
+};
+
+export default Socials;
---
+++ src/styles/socials.style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const StyledSocials = styled.div`
+  ul {
+    display: inline-flex;
+    list-style: none;
+    li {
+      padding-inline: 10px;
+      a {
+        text-decoration: none;
+        color: white;
+        &:hover {
+          color: var(--slate);
+        }
+      }
+    }
+  }
+`;
