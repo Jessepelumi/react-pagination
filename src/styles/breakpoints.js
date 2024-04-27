2db37b4fe0e3fd093978305fb57b94b46766b550
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-04-27 19:49:12 +0100

    created breakpoints for responsiveness

---
+++ src/styles/breakpoints.js
@@ -0,0 +1,22 @@
+import { css } from "styled-components";
+
+const breakpoints = {
+  xxs: "290px",
+  xs: "400px",
+  sm: "600px",
+  md: "700px",
+  lg: "1280px",
+  xl: "1440px",
+  xxl: "1920px",
+};
+
+const media = Object.keys(breakpoints).reduce((acc, key) => {
+  acc[key] = (...args) => css`
+    @media (max-width: ${breakpoints[key]}) {
+      ${css(...args)}
+    }
+  `;
+  return acc;
+}, {});
+
+export { media };
