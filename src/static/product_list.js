e644bb3fcfb744b0a925d55584357594126ebc4c
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-04-27 12:38:25 +0100

    created project list

---
+++ src/static/product_list.js
@@ -0,0 +1,27 @@
+export const product_list = [
+  {
+    id: 1,
+    product_name: "Dummy Product 0",
+    product_price: 7000,
+  },
+  {
+    id: 2,
+    product_name: "Dummy Product 1",
+    product_price: 6000,
+  },
+  {
+    id: 3,
+    product_name: "Dummy Product 2",
+    product_price: 5000,
+  },
+  {
+    id: 4,
+    product_name: "Dummy Product 3",
+    product_price: 4000,
+  },
+  {
+    id: 5,
+    product_name: "Dummy Product 4",
+    product_price: 3000,
+  },
+];
