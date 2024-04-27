a93d22e8f395b8b7217bfbff1e25e60aaf7793f4
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-04-27 19:48:51 +0100

    added colors

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -3,6 +3,12 @@ import { css } from "styled-components";
 const variables = css`
   :root {
     --font-custom: "SpaceGrotesk";
+
+    --transparent: transparent;
+    --white: #FFFFFF;
+    --creame: #F0EBE3;
+    --blue: #1679AB;
+    --slate: #161A30;
   }
 `;
 
